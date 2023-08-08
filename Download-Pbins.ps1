d66930347434749ca308c6b95d1155a6acf1c6d5
Mohammed Guiassa <guiassamohammed@gmail.com>
2023-08-08 11:20:56 +0300

    Add files via upload

---
+++ Download-Pbins.ps1
@@ -0,0 +1,29 @@
+param (
+  [Parameter(Mandatory)]
+  [string]$Destination,
+
+  [Parameter(Mandatory)]
+  [string]$StorageAccountName,
+
+  [Parameter(Mandatory)]
+  [string]$StorageAccountKey,
+
+  [Parameter(Mandatory)]
+  [string[]]$Pbins
+)
+
+Import-Module Az.Storage
+
+if (-Not (Test-Path -Path $Destination -PathType Container)) {
+  New-Item -Path $Destination -ItemType Directory
+}
+$context = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+foreach ($pbin in $Pbins) {
+  if (-Not (Test-Path -Path "$Destination/$pbin" -PathType Leaf)) {
+    Write-Host -NoNewline "Downloading $pbin... "
+    Get-AzStorageFileContent -ShareName shared-pbins -Path "$pbin" -Context $context -Destination $Destination
+    Write-Host "done"
+  } else {
+    Write-Host "Skipping $pbin (cached)"
+  }
+}
